4a57152a6a4e058a2d8e00f0e733a567bc59c42d
Khem Marriott <KhemMarriott@Gmail.com>
2017-05-24 15:00:17 -0400

    first commit

---
+++ personal_website/src/profile.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+
+class Profile extends Component {
+
+	render() {
+		return <div className='center'>
+		<div className='container'>
+
+		<div className='name center'> Khem Marriott</div>
+
+		{/*<div className='center head'>work about contact</div>*/}
+
+		<div className = 'center body'>
+
+		<br/>
+
+		<div className='center'>
+		Portfolio Coming Soon
+		</div>
+
+
+<div class="seperator">
+
+			<div class="placer">
+				
+			</div>
+			<div class="placer">
+				
+			</div>
+			<div class="placer">
+				
+			</div>
+
+		</div>
+
+		</div>
+		</div>
+		</div>
+
+
+	}
+}
+
+
+
+export default Profile
